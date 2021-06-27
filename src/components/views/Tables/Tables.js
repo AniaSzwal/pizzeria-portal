f1dd51b7f8abf75918d6543a0666e86ae510fc74
Anna Szwal <aniaszwal@gmail.com>
2021-06-27 16:01:07 +0200

    add routing

--- src/components/views/Tables/Tables.js
+++ src/components/views/Tables/Tables.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import styles from './Tables.scss';
+import {Link} from 'react-router-dom'
 
 const Tables = () => (
   <div className={styles.component}>
     <h2>Tables view</h2>
+    <Link to={`${process.env.PUBLIC_URL}/tables/booking/:id`} activeClassName='active'>Booking</Link>
+    <Link to={`${process.env.PUBLIC_URL}/tables/booking/new`} activeClassName='active'>New booking</Link>
+    <Link to={`${process.env.PUBLIC_URL}/tables/events/:id`} activeClassName='active'>Events</Link>
+    <Link to={`${process.env.PUBLIC_URL}/tables/events/new`} activeClassName='active'>New event</Link>
   </div>
 );
 
--- src/components/views/Weiter/Weiter.js
+++ src/components/views/Weiter/Weiter.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import styles from './Weiter.module.scss';
+import {Link} from "react-router-dom";
 
 const Weiter = () => (
   <div className={styles.component}>
     <h2>Weiter view</h2>
+    <Link to={`${process.env.PUBLIC_URL}/waiter/order/new`} activeClassName='active'>New order</Link>
+    <Link to={`${process.env.PUBLIC_URL}/waiter/order/:id`} activeClassName='active'>Orders</Link>
   </div>
 );
 
