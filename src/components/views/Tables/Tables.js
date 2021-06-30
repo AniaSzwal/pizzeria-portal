e33b6309520f0f6b549ecf35585f91cb04671ff8
Anna Szwal <aniaszwal@gmail.com>
2021-06-30 18:42:45 +0200

    fixed lint's bugs

--- src/components/views/Tables/Tables.js
+++ src/components/views/Tables/Tables.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styles from './Tables.scss';
-import {Link} from 'react-router-dom'
+import {Link} from 'react-router-dom';
 
 const Tables = () => (
   <div className={styles.component}>
--- src/components/views/Weiter/Weiter.js
+++ src/components/views/Weiter/Weiter.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styles from './Weiter.module.scss';
-import {Link} from "react-router-dom";
+import {Link} from 'react-router-dom';
 
 const Weiter = () => (
   <div className={styles.component}>
